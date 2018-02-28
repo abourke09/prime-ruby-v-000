737c8d7408a2b63ebebf891e90477b1cb8ea4d7c
Annie Bourke <aabourke@gmail.com>
2018-02-28 23:28:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(x)
-  
+
   range = (2..(x-1)).to_a
   answer = "true"
-  
+
   range.each do |y|
-    if x % y == 0 
+    if x % y == 0
       answer = "false"
     end
   end
-  
-  puts answer 
+
+  puts answer
 end
