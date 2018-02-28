7b0bf7b454834401ce59b8c970adf3c71662a410
Annie Bourke <aabourke@gmail.com>
2018-02-28 23:26:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,1 @@
-def prime?(x)
-  range = (2..x-1).to_a
-  
-  range.each do |y|
-    if x % y == 0 
-      puts "#{x} is evenly divisible by #{y}, therefore #{x} is NOT a prime."
-      puts "false"
-      break
-    end
-    puts "TRUE: #{x} is INDEED a prime"
-    break
-  end
-end
+# Add  code here!
